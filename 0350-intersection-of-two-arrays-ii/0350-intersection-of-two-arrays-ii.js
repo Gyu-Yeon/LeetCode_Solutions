4aaf9b93540d040b7f83c46edd9456a297358929
Johnny Choi <79036459+Gyu-Yeon@users.noreply.github.com>
2023-03-26 15:12:16 +0900

    Time: 56 ms (94.23%), Space: 42.9 MB (58.77%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+    let obj1= {}
+    for (let i = 0; i < nums1.length; i++) {
+        if (!obj1[nums1[i]]) {
+            obj1[nums1[i]] = 1
+        } else if (obj1[nums1[i]] > 0) {
+            obj1[nums1[i]] = obj1[nums1[i]] + 1
+        }
+    }
+    let answer = []
+    for (let j = 0; j <nums2.length; j++) {
+        if (obj1[nums2[j]] > 0) {
+            answer.push(nums2[j]);
+            obj1[nums2[j]] = obj1[nums2[j]] - 1
+        }
+    }
+    return answer
+};
+

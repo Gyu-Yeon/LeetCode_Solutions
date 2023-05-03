f1ee253626f183dfb5f0146637cb2da97397a091
Johnny Choi <79036459+Gyu-Yeon@users.noreply.github.com>
2023-05-04 00:09:51 +0900

    Time: 363 ms (65.96%), Space: 107 MB (12.18%) - LeetHub

---
+++ 2623-memoize/2623-memoize.js
@@ -0,0 +1,11 @@
+/**
+ * @param {Function} fn
+ */
+function memoize(fn) {
+    const map={}
+    return function(...args) {
+        const key=args.join('~')
+        if(map[key]==undefined)map[key]=fn(...args)
+        return map[key]
+    }
+}

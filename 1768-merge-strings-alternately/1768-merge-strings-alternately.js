9d97c237e94fab35e47ab7309941378b9eee7a03
Johnny Choi <79036459+Gyu-Yeon@users.noreply.github.com>
2025-01-14 12:03:30 +0900

    Time: 44 ms (95.06%), Space: 50.5 MB (14.88%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.js
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -3,37 +3,30 @@
  * @param {string} word2
  * @return {string}
  */
-var mergeAlternately = function(word1, word2) {
-    let l1 = word1.split("");
-    let l2 = word2.split("");
-    let ans = [];
-    // console.log(l1, l2)
-    let len = 0;
-    if (word1.length >= word2.length) {
-        len = word1.length;
-    } else {
-        let = word2.length;
+const calcLength = (w1, w2) => {
+        let longest;
+        if (w1.length <= w2.length) {
+            longest = w2;
+        } else {
+            longest = w1;
+        }
+        return longest.length;
     }
-    // for (let i = 0; i < len; i++) {
-    //     if(l1[i] && l2[i]) {
-    //         ans.push(l1.shift());
-    //         ans.push(l2.shift());
-    //     } else if (!l1[i] && l2[i]) {
-    //         ans.push(l2.shift());
-    //     } else if (l1[i] && !l2[i]) {
-    //         ans.push(l1.shift())
-    //     }
-    // }
-    let i = 0;
-    while (l1.length !== 0 || l2.length !== 0){
-        if(l1[i] && l2[i]) {
-            ans.push(l1.shift());
-            ans.push(l2.shift());
-        } else if (!l1[i] && l2[i]) {
-            ans.push(l2.shift());
-        } else if (l1[i] && !l2[i]) {
-            ans.push(l1.shift())
+
+var mergeAlternately = function(word1, word2) {
+    let ans = [];
+    const lengthOfLoop = calcLength(word1, word2);
+    for (let i = 0; i < lengthOfLoop; i++) {
+        if (word1[i]) {
+            ans.push(word1[i]);            
+        }
+        if (word2[i]) {
+            ans.push(word2[i]);
         }
     }
     return ans.join("");
 };
+
+// 우선 변수 ans을 만든다.
+// word1 과 words2중 길이가 더 긴 words를 기준으로 반복문을 생성한다.
+// words1의 첫번째 부터 번갈아 변수 ans에 push 한 후, 마지막에 join을 사용하여 return.
